995984501e5fa7a5944f193288e01c5c4f25b167
Pradnyesh <pradnyeshbhalekar78@gmail.com>
2026-08-20 10:34:23 +0530

    feat: add db connection and ticket model

---
+++ backend/src/models/Ticket.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema({
+  note_text: { type: String, required: true },
+  author: {type:String},
+  created_at: { type: Date, default: Date.now },
+});
+
+const ticketSchema = new mongoose.Schema(
+  {
+    ticket_id: { type: String, required: true, unique: true },
+    customer_name: { type: String, required: true },
+    customer_email: { type: String, required: true,
+      lowercase: true,
+      trim: true,
+      match:[/^\S+@\S+\.\S+$/, 'Invalid email format']
+     },
+    subject: { type: String, required: true },
+    description: { type: String, required: true },
+    status: {
+      type: String,
+      enum: ['Open', 'In Progress', 'Closed'],
+      default: 'Open',
+    },
+    notes: [noteSchema],
+  },
+  {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  }
+);
+
+module.exports = mongoose.model('Ticket', ticketSchema);
